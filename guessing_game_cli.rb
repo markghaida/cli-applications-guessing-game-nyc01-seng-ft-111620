cf0be5735daa40f227afffe45df468a9084f91ff
Mark G <markghaida@gmail.com>
2020-11-12 18:46:32 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -9,8 +9,7 @@ def run_guessing_game
     puts "You guessed the correct number!"
   elsif users_input != random_number
     puts "Sorry! The computer guessed #{random_number}."
-  elsif users_input == "exit"
-    puts "Goodbye!"
+  elsif 
   
   end 
 end 
