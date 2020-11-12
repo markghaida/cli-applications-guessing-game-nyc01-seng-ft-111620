9b3a9a2a97fd4dc5a5f66d4dc04d2c94c4b1b2c1
Mark G <markghaida@gmail.com>
2020-11-12 18:42:09 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,1 +1,15 @@
+require 'pry'
 # Code your solution here!
+def run_guessing_game
+  random_number = rand(1..6)
+  puts "Guess a number between 1 and 6!"
+  users_input = gets.chomp 
+  
+  if users_input == random_number
+    puts "You guessed the correct number!"
+  elsif users_input == "exit"
+    puts "Goodbye!"
+  elsif users_input != random_number
+    puts "Sorry! The computer guessed #{random_number}."
+  end 
+end 
--- spec/guessing_cli_spec.rb
+++ spec/guessing_cli_spec.rb
@@ -1,5 +1,5 @@
 require_relative 'spec_helper'
-
+require 'pry'
 describe "Guessing CLI" do
 
   context 'user inputs' do
